b8394b24fce8750245eb5c28e4c90deb5071a754
Aiden Nathan <agent9@y7mail.com>
2026-02-04 01:15:23 +1100

    Stringggggsssssss

--- Assets/EntityDuplicationUI.cs
+++ Assets/EntityDuplicationUI.cs
@@ -4,6 +4,7 @@ using System.IO;
 using UnityEngine;
 using TMPro;
 using SimpleFileBrowser;
+using System.Text.RegularExpressions;
 
 public class EntityDuplicationUI : MonoBehaviour
 {
@@ -12,9 +13,13 @@ public class EntityDuplicationUI : MonoBehaviour
 
     public GameObject successText;
 
-    public string modPrefix = "More_Chillies";
-
+    private string modName;
     private string entityName;
+
+    private string uniqueID => string.Format("{0}_{1}", modName, entityName);
+    private string entityNameFolder => entityName.Replace("_", "");
+    private string modNameCategory => modName.Replace("_", "");
+
     private string entityReference;
     private string outputPath;
     private bool generateModels = true;
@@ -43,6 +48,8 @@ public void Start()
     {
         successText.SetActive(false);
     }
+
+    public void SetModName(string name) { modName = name; }
 
     public void SetEntityName(string name) { entityName = name; }
 
@@ -118,7 +125,7 @@ public void Start()
             File.Create(serverLang);
         }
 
-        using (FileStream fs = new FileStream(serverLang, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        using (FileStream fs = new FileStream(serverLang, FileMode.Open, FileAccess.ReadWrite))
         {
             using (StreamReader sr = new StreamReader(fs))
             {
@@ -126,17 +133,74 @@ public void Start()
                 {
                     var text = sr.ReadToEnd();
 
-                    if (!text.Contains("items." + entityName + ".name"))
+                    if (!text.Contains("items." + uniqueID + ".name"))
                     {
-                        sw.WriteLine("items." + entityName + ".name = '");
+                        sw.WriteLine("items." + uniqueID + ".name = '");
                     }
 
-                    if (!text.Contains("items." + entityName + ".description"))
+                    if (!text.Contains("items." + uniqueID + ".description"))
                     {
-                        sw.WriteLine("items." + entityName + ".description = '");
+                        sw.WriteLine("items." + uniqueID + ".description = '");
                     }
                 }
             }
+        }
+
+        if (!Directory.Exists(itemDir))
+        {
+            Directory.CreateDirectory(itemDir);
+        }
+
+        if (!Directory.Exists(itemsDir))
+        {
+            Directory.CreateDirectory(itemsDir);
+        }
+
+        var itemPath = Path.Combine(itemsDir, uniqueID) + ".json";
+        var splitName = entityReference.Split("_");
+        var itemNameNoTag = splitName[splitName.Length - 2];
+
+        if (!File.Exists(itemPath))
+        {
+            File.Copy(entityReference, itemPath, false);
+        }
+
+        using (FileStream fs = new FileStream(itemPath, FileMode.Open, FileAccess.ReadWrite))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                var text = sr.ReadToEnd();
+
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    text = InsertBetween(text, "\"Name\": \"server.items.", ".name\",", uniqueID);
+                    text = InsertBetween(text, "\"Description\": \"server.items.", ".description\"", uniqueID);
+
+                    text = InsertBetween(text, "\"Texture\": \"Resources/", "_Texture.png\",", entityNameFolder + "/"  + entityName);
+                    text = InsertBetween(text, "Model\": \"Resources/", ".blockymodel\",", entityNameFolder + "/" + entityName);
+                    sw.Write(text);
+                }
+            }
+        }
+
+        if (!Directory.Exists(plantsDir))
+        {
+            Directory.CreateDirectory(plantsDir);
+        }
+
+        if (!Directory.Exists(seedsDir))
+        {
+            Directory.CreateDirectory(seedsDir);
+        }
+
+        if (cropType == 0 || cropType == 1) // Normal Crop
+        {
+
+        }
+
+        if (cropType == 0 || cropType == 2) // Eternal Crop
+        {
+
         }
 
         if (!Directory.Exists(commonDir))
@@ -144,18 +208,14 @@ public void Start()
             Directory.CreateDirectory(commonDir);
         }
 
-        if (cropType == 0 || cropType == 1) // Normal Crop
-        { 
-            
-        }
-
-        if (cropType == 0 || cropType == 2) // Eternal Crop
-        { 
-            
-        }
-
         successText.SetActive(true);
         yield return new WaitForSeconds(2);
         successText.SetActive(false);
+    }
+
+    public static string InsertBetween(string sourceString, string startTag, string endTag, string insert)
+    {
+        Regex regex = new Regex(string.Format("{0}(.*?){1}", Regex.Escape(startTag), Regex.Escape(endTag)), RegexOptions.RightToLeft);
+        return regex.Replace(sourceString, startTag + insert + endTag);
     }
 }
